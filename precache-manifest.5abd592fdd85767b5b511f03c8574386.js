18da0194b2519025d46a63ce49c29c5caab9bd7d
Roma Kaul <roma.s.kaul@gmail.com>
2020-06-03 18:18:09 -0700

    Updates

---
+++ precache-manifest.5abd592fdd85767b5b511f03c8574386.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "209b6ad54e599b484d16eaa55f949a22",
+    "url": "/mockup/index.html"
+  },
+  {
+    "revision": "a18389b9953cafc96352",
+    "url": "/mockup/static/css/main.e7ab57c1.chunk.css"
+  },
+  {
+    "revision": "2b06a671664a07625c37",
+    "url": "/mockup/static/js/2.ea42d2e8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/mockup/static/js/2.ea42d2e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a18389b9953cafc96352",
+    "url": "/mockup/static/js/main.b84edeed.chunk.js"
+  },
+  {
+    "revision": "8b9f8426c2dff192e2f1",
+    "url": "/mockup/static/js/runtime-main.6c5093ef.js"
+  }
+]);
